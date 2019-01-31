a1b9cc0dd0955f764445a1ca9f6ffac00e30d5bd
Owersun <owersun@gmail.com>
2019-01-31 23:45:07 +0300

    Refactor image building process to produce one partition, 1Gb image ready to be flashed to SD card

--- buildme.sh
+++ buildme.sh
@@ -2,7 +2,7 @@
 
 
 echo "Checking all required tools are installed"
-TOOLS="wget tar git make 7z unsquashfs dd vim mkfs.ext4 sudo parted mkdosfs mcopy dtc"
+TOOLS="wget tar git make 7z unsquashfs dd mkfs.ext4 parted dtc"
 
 for i in $TOOLS; do
 	TOOL_PATH=`which $i`
@@ -12,12 +12,14 @@ for i in $TOOLS; do
 	fi
 done
 
+SUDO=`if [ $UID -ne 0 ]; then echo 'sudo'; fi`
+
 GCCDIR=7.4-2019.02
 GCCVER=7.4.1-2019.02
 
 ROOTDIR=`pwd`
 if [[ ! -d $ROOTDIR/build/toolchain/gcc-linaro-$GCCVER-x86_64_aarch64-linux-gnu ]]; then
-	mkdir -p $ROOTDIR/build/toolchain
+	mkdir -vp $ROOTDIR/build/toolchain
 	cd $ROOTDIR/build/toolchain
         wget https://releases.linaro.org/components/toolchain/binaries/$GCCDIR/aarch64-linux-gnu/gcc-linaro-$GCCVER-x86_64_aarch64-linux-gnu.tar.xz
         tar -xvf gcc-linaro-$GCCVER-x86_64_aarch64-linux-gnu.tar.xz
@@ -38,6 +40,10 @@ export ATF_BRANCH=atf-v1.3-armada-17.10
 
 export MVDDR_BRANCH=mv_ddr-armada-17.10
 
+export KERNELDIR=linux-marvell
+export KERNEL_REPO=https://github.com/MarvellEmbeddedProcessors/linux-marvell
+export KERNEL_BRANCH=linux-4.4.52-armada-17.10
+
 export PATH=$PATH:$ROOTDIR/build/toolchain/gcc-linaro-$GCCVER-x86_64_aarch64-linux-gnu/bin
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
@@ -45,9 +51,9 @@ export ARCH=arm64
 # ATF specific defines
 export SCP_BL2=$ROOTDIR/build/bootloader/binaries-marvell/mrvl_scp_bl2_8040.img
 export MV_DDR_PATH=$ROOTDIR/build/bootloader/mv-ddr-marvell
-export BL33=$ROOTDIR/build/bootloader/u-boot-marvell/u-boot.bin
+export BL33=$ROOTDIR/build/bootloader/$UBOOTDIR/u-boot.bin
 
-echo "Building boot loader"
+echo "Downloading boot loader"
 cd $ROOTDIR
 mkdir -vp build/bootloader
 if [[ ! -d $ROOTDIR/build/bootloader/$UBOOTDIR ]]; then
@@ -56,7 +62,7 @@ if [[ ! -d $ROOTDIR/build/bootloader/$UBOOTDIR ]]; then
 	cd $UBOOTDIR
 	git am $ROOTDIR/patches/u-boot/*
 else
-        cd $UBOOTDIR
+        cd $ROOTDIR/build/bootloader/$UBOOTDIR
         git pull
         git branch -v
 fi
@@ -90,40 +96,10 @@ else
 	git branch -v
 fi
 
-if [[ ! -d $ROOTDIR/build/linux-marvell ]]; then
-	cd $ROOTDIR/build/
-	git clone https://github.com/MarvellEmbeddedProcessors/linux-marvell
-	cd linux-marvell
-	git checkout linux-4.4.52-armada-17.10
-	git am $ROOTDIR/patches/kernel/*
-fi
-
-if [[ ! -f $ROOTDIR/build/ubuntu-18.04/ext4.part ]]; then
-	cd $ROOTDIR/build/
-	mkdir -p ubuntu-18.04
-	cd ubuntu-18.04
-	if [[ ! -f ubuntu-18.04.1-server-arm64.iso ]]; then
-		wget http://cdimage.ubuntu.com/releases/18.04/release/ubuntu-18.04.1-server-arm64.iso
-	fi
-	rm -rf install/filesystem.squashfs
-	7z x ubuntu-18.04.1-server-arm64.iso install/filesystem.squashfs
-	# The following command requires sudo... sorry
-	sudo unsquashfs -d temp/ install/filesystem.squashfs
-	# Manuall remove the 'x' from the root passwd
-	sudo vim temp/etc/passwd
-	# Create a sparse 1GB file
-	sudo dd if=/dev/zero of=ext4.part bs=1 count=0 seek=300M
-	sudo mkfs.ext4 -b 4096 ext4.part
-	sudo mount -o loop ext4.part /mnt/
-	sudo cp -a temp/* /mnt/
-	sudo umount /mnt/
-fi
-
-echo "Building u-boot"
-cd $ROOTDIR/build/bootloader/u-boot-marvell
+echo "Building U-Boot"
+cd $ROOTDIR/build/bootloader/$UBOOTDIR
 make solidrun_cf_gt_8k_defconfig
 make
-
 if [ $? != 0 ]; then
 	echo "Error building u-boot"
 	exit -1
@@ -137,8 +113,20 @@ if [ $? != 0 ]; then
 	exit -1
 fi
 
-echo "Building kernel"
-cd $ROOTDIR/build/linux-marvell
+echo "Downloading Linux kernel"
+if [[ ! -d $ROOTDIR/build/$KERNELDIR ]]; then
+        cd $ROOTDIR/build/
+        git clone --branch=$KERNEL_BRANCH $KERNEL_REPO $KERNELDIR
+	cd $KERNELDIR
+        git am $ROOTDIR/patches/kernel/*
+else
+	cd $ROOTDIR/build/$KERNELDIR
+	git pull
+	git branch -v
+fi
+
+echo "Building Kernel"
+cd $ROOTDIR/build/$KERNELDIR
 make mvebu_v8_lsp_defconfig
 ./scripts/kconfig/merge_config.sh .config $ROOTDIR/configs/extra.config
 make -j4
@@ -147,39 +135,48 @@ if [ $? != 0 ]; then
 	exit -1
 fi
 
-echo "Done building... Now copy images"
-cd $ROOTDIR/
-mkdir -p images
-cp build/bootloader/atf-marvell/build/a80x0_cf_gt_8k/release/flash-image.bin images/
-cp build/linux-marvell/arch/arm64/boot/Image images/
-cp build/linux-marvell/arch/arm64/boot/dts/marvell/armada-8040-clearfog-gt-8k.dtb images/
-cd build/linux-marvell/; make INSTALL_MOD_PATH=$ROOTDIR/images/modules/ modules_install
-cd $ROOTDIR/
-cp build/buildroot/output/images/rootfs.cpio.uboot images
+echo "Downloading Ubuntu Image"
+if [[ ! -f $ROOTDIR/build/ubuntu-18.04.1-server-arm64.squashfs ]]; then
+        cd $ROOTDIR/build
+        if [[ ! -f ubuntu-18.04.1-server-arm64.iso ]]; then
+                wget http://cdimage.ubuntu.com/releases/18.04/release/ubuntu-18.04.1-server-arm64.iso
+        fi
+        7z x ubuntu-18.04.1-server-arm64.iso install/filesystem.squashfs
+	mv install/filesystem.squashfs ubuntu-18.04.1-server-arm64.squashfs
+fi
+
+cd $ROOTDIR
 
 echo "Creating partitions and images"
-dd if=images/flash-image.bin of=images/microsd.img bs=512 seek=1
-dd if=/dev/zero of=images/disk.img bs=1M count=401
-parted --script images/disk.img mklabel msdos mkpart primary 1MiB 100MiB mkpart primary 100MiB 400MiB
-#parted --script images/disk.img mklabel gpt mkpart primary 1MiB 100MiB mkpart primary 100MiB 400MiB
-dd if=/dev/zero of=images/boot.part bs=1M count=99
+dd if=/dev/zero of=$ROOTDIR/image.img bs=1M count=1024
+parted --script $ROOTDIR/image.img mklabel msdos mkpart primary 4096s 100%
 
-# Start with boot partition
-mkdosfs images/boot.part
-mcopy -i images/boot.part images/Image ::/Image
-mcopy -i images/boot.part images/armada-8040-clearfog-gt-8k.dtb ::/armada-8040-clearfog-gt-8k.dtb
-cd images/modules/; tar Jc lib > $ROOTDIR/images/modules.tar.xz; cd -
-mcopy -i images/boot.part images/modules.tar.xz ::/modules.tar.xz
-# Create a uenv.txt for the boot partition
-cat > images/uenv.txt <<EOF
+echo "Fusin bootloader to the image"
+dd if=$ROOTDIR/build/bootloader/atf-marvell/build/a80x0_cf_gt_8k/release/flash-image.bin of=image.img conv=notrunc bs=512 seek=1
+
+echo "Filling image with data"
+mkdir -pv $ROOTDIR/image
+$SUDO losetup -o 4096 /dev/loop0 $ROOTDIR/image.img
+$SUDO mkfs.ext4 /dev/loop0
+$SUDO mount /dev/loop0 $ROOTDIR/image
+
+echo "Copying filesystem to the image"
+$SUDO unsquashfs -d $ROOTDIR/image/ -f $ROOTDIR/build/ubuntu-18.04.1-server-arm64.squashfs
+
+echo "Copying kernel to the image"
+cp -av $ROOTDIR/build/$KERNELDIR/arch/arm64/boot/Image $ROOTDIR/image/boot/
+cp -av $ROOTDIR/build/$KERNELDIR/arch/arm64/boot/dts/marvell/armada-8040-clearfog-gt-8k.dtb $ROOTDIR/image/boot/
+cat > $ROOTDIR/image/boot/uenv.txt <<EOF
 bootargs=console=ttyS0,115200 root=/dev/sda2 rw
 uenvcmd=fatload scsi 0:1 0x02000000 Image; fatload scsi 0:1 0x01800000 armada-8040-clearfog-gt-8k.dtb; booti 0x02000000 - 0x01800000
 EOF
-mcopy -i images/boot.part images/uenv.txt ::/uenv.txt
-dd if=images/boot.part of=images/disk.img bs=1M seek=1 conv=notrunc
+cd $ROOTDIR/build/$KERNELDIR && make INSTALL_MOD_PATH=$ROOTDIR/image/ modules_install
+$SUDO chown -R root:root $ROOTDIR/image/boot $ROOTDIR/image/lib/modules
 
-#Copy over the rootfs partition
-dd if=build/ubuntu-16.04/ext4.part of=images/disk.img bs=512 seek=204800 conv=notrunc
+echo "Finishing..."
+$SUDO umount $ROOTDIR/image
+$SUDO losetup -d /dev/loop0
 
-#boot_ssd=scsi reset; setenv bootargs console=ttyS0,115200 root=/dev/sda2 rw; fatload scsi 0:1 0x02000000 /Image; fatload scsi 0:1 0x01800000 armada-8040-clearfog-gt-8k.dtb; booti 0x02000000 - 0x01800000
+echo "Done."
+cd $ROOTDIR
 
